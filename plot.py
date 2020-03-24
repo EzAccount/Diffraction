d8d55413cac491f0cd4081e466f800b677e0116c
Mikhail Tikhonov <me@mtikhonov.com>
2020-03-24 17:15:27 +0300

    Data files cleared

--- plot.py
+++ plot.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 x = np.array([])
 y = np.array([])
 
-with open("ExperimentHole.out") as f:
+with open("data/NS_range1e-05step2.5e-07scaling1diversity0.2blurry_x.out") as f:
     for line in f:
-        temp_x, temp_y = [float(x) for x in line.split()]
+        temp_x, temp_y = [float(num) for num in line.split()]
         x = np.append(x, temp_x)
         y = np.append(y, temp_y)
+print(np.size(x), np.size(y))
 plt.plot(x,y)
+
 plt.show()
